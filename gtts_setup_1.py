b4c321763b834326a847531115bde1cd817a651e
Subhadeep Kanjilal <subhadeep2006kanjilal@gmail.com>
2026-03-19 20:02:31 +0530

    First commit

---
+++ gtts_setup_1.py
@@ -0,0 +1,21 @@
+"""from gtts import gTTS
+import os
+
+text = "আমার সোনার বাংলা"  # Your Bengali text here
+tts = gTTS(text=text, lang='bn')
+tts.save("output.mp3")
+os.system("start output.mp3")  # plays on Windows
+
+"""
+
+import streamlit as st
+from gtts import gTTS
+
+st.title("Bengali TTS - Prototype")
+text = st.text_area("Type Bengali text here")
+
+if st.button("Generate Audio"):
+    tts = gTTS(text=text, lang='bn')
+    tts.save("output.mp3")
+    st.audio("output.mp3")
+    st.download_button("Download", open("output.mp3", "rb"), "output.mp3")
